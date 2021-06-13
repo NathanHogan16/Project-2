a9358b35ebac0e7d36f14d74325d7ce06aec0d66
Nate Hogan <natek.hogan@gmail.com>
2021-06-13 15:37:46 -0600

    trying to get herok to work

--- config/connection.js
+++ config/connection.js
@@ -1,33 +1,27 @@
 // import the Sequelize constructor from the library
-const { Sequelize } = require('sequelize');
+// const { Sequelize } = require('sequelize');
 
-require('dotenv').config();
+// require('dotenv').config();
 
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-      host: 'localhost',
-      dialect: 'mysql',
-      port: 3306
-    });
+// const sequelize = process.env.JAWSDB_URL
+//   ? new Sequelize(process.env.JAWSDB_URL)
+//   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+//       host: 'localhost',
+//       dialect: 'mysql',
+//       port: 3306
+//     });
 
-// const sequelize = new Sequelize('recipes', 'root', 'root', {
-//   host: 'localhost',
-//   port: 3306,
-//   dialect: 'mysql'
-// });
-
-module.exports = sequelize;
+// module.exports = sequelize;
 
 
-// let sequelize;
+let sequelize;
 
-// if (process.env.JAWSDB_URL) {
-//   sequelize = new Sequelize(process.env.JAWSDB_URL);
-// } else {
-//   sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-//     host: 'localhost',
-//     dialect: 'mysql',
-//     port: 3306
-//   });
-// }
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3306
+  });
+}
