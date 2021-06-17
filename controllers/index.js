b49a8d98c81f19d3d71cd0e6d55d05d88a1be1ba
Nate Hogan <natek.hogan@gmail.com>
2021-06-16 20:25:22 -0600

    tests

--- controllers/index.js
+++ controllers/index.js
@@ -1,14 +1,12 @@
 
 const router = require('express').Router();
  const apiRoutes = require('./api');
-// const homeRoutes = require('./home-routes');
+ const homeRoutes = require('./home-routes');
 
-//const userRoutes = require('./user-routes.js');
 
-//router.use('/users', userRoutes);
-router.get("/",(req, res) => res.send("hello3"))
-// router.use('/api', apiRoutes);
-// router.use('/', homeRoutes)
+
+router.use('/api', apiRoutes);
+router.use('/', homeRoutes)
 
 router.use((req, res) => {
     res.status(404).end();
--- server.js
+++ server.js
@@ -2,25 +2,25 @@ const express = require('express');
 
  const exphbs = require('express-handlebars')
 
-// const sequelize = require('./config/connection');
+ const sequelize = require('./config/connection');
  const routes = require('./controllers');
-// const path = require('path');
-// const  session = require('express-session');
+const path = require('path');
+const  session = require('express-session');
 
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-//const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-// const sess = {
-//   secret: 'Super secret secret',
-//   cookie: {},
-//   resave: false,
-//   saveUninitialized: true,
-//   store: new SequelizeStore({
-//     db: sequelize
-//   })
-// };
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
 
  const helpers = require('./utils/helpers');
  const hbs = exphbs.create({ helpers });
@@ -28,7 +28,7 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 // Parsing
 app.use(express.urlencoded({ extended: false }));
-//app.use(session(sess));
+app.use(session(sess));
 
 app.use(express.static("public"));
 
@@ -39,9 +39,9 @@ app.set('view engine', 'handlebars');
 
 
 
-// app.engine('handlebars', exphbs({
-//   layoutsDir: __dirname + '/views/layouts',
-// }));
+app.engine('handlebars', exphbs({
+  layoutsDir: __dirname + '/views/layouts',
+}));
 
 
 //app.use(require('./controllers/'));
@@ -50,8 +50,8 @@ app.set('view engine', 'handlebars');
 
  
 
- //sequelize.sync({ force: false }).then(() => {
+ sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, function() {
       console.log('App listening on PORT ' + PORT);
     });
- //});
+ });
