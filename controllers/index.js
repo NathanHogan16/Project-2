56465e2ee43351a22eff6e5990cd4edc3418c886
Nate Hogan <natek.hogan@gmail.com>
2021-06-16 20:11:22 -0600

    tests

--- controllers/index.js
+++ controllers/index.js
@@ -1,12 +1,12 @@
 
 const router = require('express').Router();
-// const apiRoutes = require('./api');
+ const apiRoutes = require('./api');
 // const homeRoutes = require('./home-routes');
 
 //const userRoutes = require('./user-routes.js');
 
 //router.use('/users', userRoutes);
-router.get("/",(req, res) => res.send("hello"))
+router.get("/",(req, res) => res.send("hello3"))
 // router.use('/api', apiRoutes);
 // router.use('/', homeRoutes)
 
